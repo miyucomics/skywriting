85bd10f8234401845c7e0b2ddfb42ed7120533a5
miyucomics <nbriannyc@gmail.com>
2025-05-06 22:08:28 -0400

    Code overhaul

--- src/main/java/miyucomics/skywriting/SkywritingClient.java
+++ src/main/java/miyucomics/skywriting/SkywritingClient.java
@@ -1,18 +1,18 @@
 package miyucomics.skywriting;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class SkywritingClient implements ClientModInitializer {
+	public static final Identifier STROKE_SPAWN_CHANNEL = new Identifier(SkywritingMain.MOD_ID, "stroke_spawn");
+	public static final Identifier STROKE_UPDATE_CHANNEL = new Identifier(SkywritingMain.MOD_ID, "stroke_update");
+
 	@Override
 	public void onInitializeClient() {
 		EntityRendererRegistry.register(SkywritingMain.STROKE_ENTITY, StrokeRenderer::new);
+		ClientPlayNetworking.registerGlobalReceiver(STROKE_SPAWN_CHANNEL, StrokeEntity::handleStrokeSpawn);
+		ClientPlayNetworking.registerGlobalReceiver(STROKE_UPDATE_CHANNEL, StrokeEntity::handleStrokeUpdate);
 	}
 }
